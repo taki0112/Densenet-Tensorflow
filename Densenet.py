843451919f99e3cb2b013acf77aeccbec9384b44
Junho Kim <takis0112@gmail.com>
2017-08-08 14:08:47 +0900

    add Densenet

--- Densenet.py
+++ Densenet.py
@@ -1,8 +1,5 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-import tensorflow.contrib.keras as keras
-
-keras.layers.GlobalAveragePooling2D()
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
